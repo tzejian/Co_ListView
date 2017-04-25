3885288b68d197316c58830cca0811024cd82764
15017135 <15017135@myrp.edu.sg>
2017-04-25 12:45:57 +0800

    first page

--- app/src/main/java/com/example/a127107/co_listview/MainActivity.java
+++ app/src/main/java/com/example/a127107/co_listview/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.a127107.co_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Year> year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) this.findViewById(R.id.lvYear);
+
+        // Create a few food objects in Food array
+        year = new ArrayList<Year>();
+        year.add(new Year("Year 1"));
+        year.add(new Year("Year 2"));
+        year.add(new Year("Year 3"));
+
+        // Link this Activity object, the row.xml layout for
+        //  each row and the food String array together
+        aa = new YearAdapter(this, R.layout.firstactivitylistview, year);
+        lv.setAdapter(aa);
+
     }
 }
